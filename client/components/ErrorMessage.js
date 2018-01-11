49d9cf3226154de113b5b46c151c82579370b383
Sandy Mak <sandy.mak91@gmail.com>
2018-01-11 12:23:58 -0500

    add SingleProduct view and ErrorMessage view

---
+++ client/components/ErrorMessage.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+const ErrorMessage = () => {
+    return (
+      <div>
+          <h2>404 Page not Found...</h2>
+          <p>(probably because we don't have this product yet...)</p>
+          <img src="https://kottke.org/plus/misc/images/failwhale.jpg
+          " style={{width: 100 + '%'}} />
+      </div>
+    )
+}
+
+export default ErrorMessage;
+
+
+
